0664eb7fcdddeda6f2e9964ae45df0584519d939
Javier Álvarez Medina <xavijam@gmail.com>
2015-07-07 01:46:12 +0200

    First app try

---
+++ README.md
@@ -0,0 +1,11 @@
+TrendMap social accounts
+========================
+
+In a [CartoDB leapfrog ](http://blog.cartodb.com/TrendMap/) we decided to
+create the "new Chrome tab" where visualize the most visited CartoDB map
+of the moment. We refresh the info every 20 minutes checking which one is
+trendy.
+
+But nowadays this is not enough, map makers want to know that through social
+networks, as in Twitter, Facebook,...etc. This idea comes from @iriberri,
+thanks lady!
---
+++ app.js
@@ -0,0 +1,154 @@
+var Twitter = require('twitter');
+var config = require('config-heroku');
+var CartoDB = require('cartodb');
+var fs = require('fs');
+var util = require('util');
+var http = require('http');
+var seqqueue = require('seq-queue');
+var request = require('request');
+var INTERVAL = 1200000;
+
+ 
+var twitterClient = new Twitter({
+  consumer_key: config['Twitter'].api_key,
+  consumer_secret: config['Twitter'].api_secret,
+  access_token_key: config['Twitter'].access_token,
+  access_token_secret: config['Twitter'].access_token_secret
+});
+
+var cartodbWriteClient = new CartoDB({
+  user: config['CartoDB'].write_username,
+  api_key: config['CartoDB'].write_api_key
+});
+
+var cartodbReadClient = new CartoDB({
+  user: config['CartoDB'].read_username
+});
+
+function getVizJson(callback) {
+  cartodbReadClient.query("SELECT url FROM {table} ORDER BY count DESC LIMIT 1", { table: 'top_viz' }, function(err, data){
+    if (data.rows && data.rows.length === 1) {
+      var url = data.rows[0].url;
+      var data = /https:\/\/(.+)\.cartodb.com\/api\/v2\/viz\/(.+)\/viz.json/.exec(url);
+      callback && callback({ username: data[1], id: data[2] });
+    }
+  });
+}
+
+function mapReported(data, callback) {
+  cartodbWriteClient.query("SELECT CASE WHEN exists (SELECT true FROM {table} WHERE map_id='{id}') THEN 'true' ELSE 'false' END", { table: 'trendy_maps_list', id: data.id }, function(err, data){
+    var reported = data && data.rows && data.rows[0].case === 'true';
+    callback && callback(reported);
+  });
+}
+
+function insertMapId(data, callback) {
+  cartodbWriteClient.query("INSERT INTO {table} (map_id) VALUES ('{id}');", { table: 'trendy_maps_list', id: data.id }, function(err, data){
+    callback && callback();
+  });
+}
+
+function postTweet(mapData, callback) {
+  var msg = 'Currently CartoDB top map: http://' + mapData.username + '.cartodb.com/viz/' + mapData.id + '/public_map';
+  
+  twitterClient.post('statuses/update', { status: msg },  function(error, tweet, response){
+    callback && callback();
+  });
+}
+
+function changeBackground(mapData, callback) {
+
+  var download = function(uri, filename, callback){
+    request.head(uri, function(err, res, body){
+      var r = request(uri).pipe(fs.createWriteStream(filename));
+      r.on('close', callback);
+    });
+  };
+
+  var img = 'http://' + mapData.username + '.cartodb.com/api/v2/viz/' + mapData.id + '/static/1400/300.png';
+
+  download(img, 'bkg.png', function(){
+    var data = require('fs').readFileSync('bkg.png');
+    twitterClient.post('account/update_profile_banner', { banner: data, media: data },  function(error, tweet, response){
+      callback && callback();
+    });
+  });
+}
+
+
+setInterval(function() {
+  var queue = seqqueue.createQueue(10000);
+  var mapData = {};
+
+  // Get trend map!
+  queue.push(
+    function(task) {
+      getVizJson(function(d) {
+        mapData = d;
+        task.done();
+      });
+    }, 
+    function() {}, 
+    1000
+  );
+
+  // Already reported?
+  queue.push(
+    function(task) {
+      mapReported(mapData, function(reported) {
+        mapData.reported = reported;
+        task.done();
+      });
+    },
+    function() {},
+    1000
+  );
+
+  // Store it if it was not reported
+  queue.push(
+    function(task) {
+      if (mapData.reported) {
+        task.done();
+      } else {
+        insertMapId(mapData, function() {
+          task.done();
+        })
+      }
+    },
+    function() {},
+    1000
+  );
+
+  // Send tweet if it was not reported
+  queue.push(
+    function(task) {
+      if (mapData.reported) {
+        task.done();
+      } else {
+        postTweet(mapData, function() {
+          task.done();
+        });
+      }
+    },
+    function() {},
+    1000
+  );
+
+  // Change background
+  queue.push(
+    function(task) {
+      if (mapData.reported) {
+        task.done();
+      } else {
+        changeBackground(mapData, function() {
+          task.done();
+        });
+      }
+    },
+    function() {},
+    6000
+  );
+
+}, INTERVAL);
+
+http.createServer().listen(8000);
