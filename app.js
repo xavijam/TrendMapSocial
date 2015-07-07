5d64450397cdb177c2eb55eb36e2b2ec2dbc1b85
Javier Álvarez Medina <xavijam@gmail.com>
2015-07-07 02:04:10 +0200

    Adding express for... nothing

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var util = require('util');
 var http = require('http');
 var seqqueue = require('seq-queue');
 var request = require('request');
+var express = require('express')
+var app = express();
 var INTERVAL = 1200000;
 
  
@@ -151,4 +153,15 @@ setInterval(function() {
 
 }, INTERVAL);
 
-http.createServer().listen(8000);
+
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Working!')
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
